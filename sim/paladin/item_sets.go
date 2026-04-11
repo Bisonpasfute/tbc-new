292cf228d97614a5c04a15d97078bc3aab7ebab5
Johan Hillerström <progr@mmer.nu>
2026-04-12 00:55:06 +0200

    [Ret] Justicar Battlegear 4pc should be additive

--- sim/paladin/item_sets.go
+++ sim/paladin/item_sets.go
@@ -4,19 +4,22 @@ import (
 	"github.com/wowsims/tbc/sim/core"
 )
 
+// T4 Ret - Justicar Battlegear
+// (2) Set: Increases the damage bonus of your Judgement of the Crusader by 15%.
+// (4) Set: Increases the damage dealt by your Judgement of Command by 10%.
 var ItemSetJusticarBattlegear = core.NewItemSet(core.ItemSet{
 	ID:   626,
 	Name: "Justicar Battlegear",
 	Bonuses: map[int32]core.ApplySetBonus{
 		2: func(agent core.Agent, setBonusAura *core.Aura) {
-
+			setBonusAura.ExposeToAPL(37186)
 		},
 		4: func(agent core.Agent, setBonusAura *core.Aura) {
 			setBonusAura.AttachSpellMod(core.SpellModConfig{
-				Kind:       core.SpellMod_DamageDone_Pct,
+				Kind:       core.SpellMod_DamageDone_Flat,
 				FloatValue: 0.10,
 				ClassMask:  SpellMaskJudgementOfCommand,
-			})
+			}).ExposeToAPL(37187)
 		},
 	},
 })
